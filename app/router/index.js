dd8f8734b5a96dd19737233f994611b80ca871bf
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-09-24 11:20:38 -0700

    Added missing logic to load all routes files

--- app/router/index.js
+++ app/router/index.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const router = express.Router();
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 // Health check
 router.get('/v1/healthcheck', async (req, res) => res.status(200).send('<img src ="https://http.cat/200"/>'));
+
+const getDirectories = (path) => fs.readdirSync(path, { withFileTypes: true })
+  .filter((file) => file.isDirectory())
+  .map((dir) => dir.name);
+
+const basePath = path.join(dirname, '../components');
+const cleanDirectories = getDirectories(basePath);
+
+// Automatically load all "routes" files found under their respective component folders
+cleanDirectories.forEach((componentName) => {
+  const routesPath = `${basePath}/${componentName}/routes.js`;
+  if (fs.existsSync(routesPath)) import(routesPath);
+});
 
 export default router;
 
