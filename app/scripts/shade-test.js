ea89170456476917fca818dccc7b6ec01e776dd9
Chris Gitmed <chris@Chriss-MacBook-Pro.local>
2022-09-25 16:56:11 -0700

    Installed js-shade (my first npm package) and tested

---
+++ app/scripts/shade-test.js
@@ -0,0 +1,6 @@
+import { Shade } from 'js-shade';
+console.log('Shade: ', Shade);
+
+console.log(Shade.red().underline('Testing'));
+console.log(Shade.underline().red('red'));
+console.log(Shade.bright().yellow('Bright yellow~'));
